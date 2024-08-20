f619ac87cfb03aa3bd42bb11ce8a2fff72874027
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-08-20 08:35:56 -0300

    Adding database access

--- src/model/services/TeacherService.java
+++ src/model/services/TeacherService.java
@@ -1,17 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.TeacherDao;
 import model.entities.Teacher;
 
 public class TeacherService {
 	
+	private TeacherDao dao = DaoFactory.createTeacherDao();
+	
 	public List<Teacher> findAll(){
-		List<Teacher> list = new ArrayList<>();
-		list.add(new Teacher(1, "Francisco", new Date(), "12345678956", "98765423", 12000.00 ));
-		list.add(new Teacher(2, "Maria", new Date(), "12345645321", "984356233", 15000.00 ));		
-		return list;
+		return dao.findAll();
 	}
 }
